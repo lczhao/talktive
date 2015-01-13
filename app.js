8c445be481a069032d555dc058c895a56821f8fa
lczhao1994 <lczhao1994@hotmail.com>
2015-01-13 13:53:08 -0500

    setup git

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app = express();
+
+app.use("/", express.static(__dirname + "\\app"));
+
+app.listen(3000, function() {
+	console.log("listening at port 3000")
+});
